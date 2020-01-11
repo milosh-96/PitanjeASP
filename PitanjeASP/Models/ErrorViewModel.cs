caeed5efa3b550de30ca43b0604a31830befd57c
milosh-96 <milosjovanovic042@gmail.com>
2020-01-12 00:55:39 +0100

    init

---
+++ PitanjeASP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PitanjeASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
